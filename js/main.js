1baafd39b4c99c2a9e24f72e9a9c01698fa6a8f5
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-02-24 19:45:24 +0100

    add new flower and different scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-// cargar toda la pagina
-
-function start() {
-  mar = document.querySelector(".mar");
-  bosque = document.querySelector(".bosque");
+// funcion para modificar las imagenes
+function parallax() {
+  flowerOne.style.top = -(pageYOffset / 20) + "px";
+  flowerTwo.style.top = -(pageYOffset / 5) + "px";
+  flowerFour.style.top = -(pageYOffset / 1) + "px";
 }
 
+function start() {
+  flowerOne = document.querySelector(".flowerOne");
+  flowerTwo = document.querySelector(".flowerTwo");
+  flowerFour = document.querySelector(".flowerFour");
+}
+
+// listener que llama a la funcion parallax, donde se cambia el style
+window.addEventListener("scroll", parallax);
+
+// listener a la pantlla al inicializar
 window.addEventListener("load", start);
