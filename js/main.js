8b9c8d6643bfcb785c8b8c94eaf536eea85847a2
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-02-24 21:41:48 +0100

    try to fix javascript

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-alert("HOla");
-
 // funcion para modificar las imagenes
-// function parallax() {
-//   flowerOne.style.top = -(pageYOffset / 20) + "px";
-//   flowerTwo.style.top = -(pageYOffset / 5) + "px";
-//   flowerFour.style.top = -(pageYOffset / 1) + "px";
-// }
+function parallax() {
+  flowerOne.style.top = -(pageYOffset / 20) + "px";
+  flowerTwo.style.top = -(pageYOffset / 5) + "px";
+  flowerFour.style.top = -(pageYOffset / 1) + "px";
+}
 
-// function start() {
-//   flowerOne = document.querySelector(".flowerOne");
-//   flowerTwo = document.querySelector(".flowerTwo");
-//   flowerFour = document.querySelector(".flowerFour");
-// }
+function start() {
+  flowerOne = document.querySelector(".flowerOne");
+  flowerTwo = document.querySelector(".flowerTwo");
+  flowerFour = document.querySelector(".flowerFour");
+}
 
 // // listener que llama a la funcion parallax, donde se cambia el style
-// window.addEventListener("scroll", parallax);
+window.addEventListener("scroll", parallax);
 
 // // listener a la pantlla al inicializar
-// window.addEventListener("load", start);
+window.addEventListener("load", start);
